386fcb4c689c9e0d4414523cb86fb937018e5961
Gabriel Garcia <g.garcia@DELL-33.seedgroup.local>
2025-04-28 13:11:17 -0600

    Config 2

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,25 +1,18 @@
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
-const fs = require('fs');
-
 dotenv.config();
 
 let serviceAccount;
 
 if (process.env.GOOGLE_CREDENTIALS) {
   try {
-    // Detecta si el contenido ya tiene \\n o solo \n
-    const credString = process.env.GOOGLE_CREDENTIALS.replace(/__LINE__/g, '\n')//process.env.GOOGLE_CREDENTIALS.includes('\\n')
-      ? process.env.GOOGLE_CREDENTIALS.replace(/\\n/g, '\n') // Render u otros
-      : process.env.GOOGLE_CREDENTIALS;                     // Local mal configurado
-
-    serviceAccount = JSON.parse(credString);
+    const fixedCredentials = process.env.GOOGLE_CREDENTIALS.replace(/__LINE__/g, '\n');
+    serviceAccount = JSON.parse(fixedCredentials);
   } catch (err) {
     console.error("❌ Error al parsear GOOGLE_CREDENTIALS:", err);
     process.exit(1);
   }
 } else {
-  // Entorno local seguro con archivo JSON
   serviceAccount = require('../inventario-11b49-firebase-adminsdk-fbsvc-2631f257be.json');
 }
 
