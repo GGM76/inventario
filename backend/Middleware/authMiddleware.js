55a904c77061304ff59027aaddcba62d9728a3f7
Gabriel Garcia <g.garcia@DELL-33.seedgroup.local>
2025-04-30 12:04:44 -0600

    Revisando el back 1

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -2,27 +2,32 @@ const { admin } = require('../config/firebase');  // Accede correctamente a `adm
 
 const authenticateToken = async (req, res, next) => {
   const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-  console.log('Firebase initialized for:', serviceAccount.project_id);
-  console.log('Token recibido:', token);
+  console.log('Token recibido:', token);  // Log del token que llega
 
   if (!token) {
+    console.log('No se proporcionó un token.');
     return res.status(401).json({ error: 'Token no proporcionado.' });
   }
 
   try {
     // Verificar token
+    console.log('Verificando token...');
     const decodedToken = await admin.auth().verifyIdToken(token);  
-    //console.log('Token verificado:', decodedToken);
+    console.log('Token verificado:', decodedToken);  // Log del token verificado
 
     // Buscar el usuario en Firestore para obtener rol y empresa
+    console.log('Buscando usuario en Firestore con UID:', decodedToken.uid);
     const userRef = admin.firestore().collection('users').doc(decodedToken.uid);
     const userDoc = await userRef.get();
     
     if (!userDoc.exists) {
+      console.log('Usuario no encontrado en Firestore.');
       return res.status(404).json({ error: 'Usuario no encontrado.' });
     }
 
     const userData = userDoc.data();
+    console.log('Datos del usuario obtenidos:', userData);  // Log de los datos del usuario
+
     req.user = { ...decodedToken, role: userData.role, empresa: userData.empresa_id }; 
     next();  // Continuar a la siguiente función (controlador)
   } catch (error) {
@@ -33,6 +38,7 @@ const authenticateToken = async (req, res, next) => {
 
 const checkAdmin = (req, res, next) => {
   if (req.user.role !== 'admin') {
+    console.log('El usuario no es administrador.');
     return res.status(403).json({ error: 'No tienes permisos para acceder a esta ruta.' });
   }
   next();  // Si es admin, continuar con la siguiente función
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const subprojectRoutes = require('./routes/subprojectRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    
 
 app.use('/roomies/', authRoutes);
 app.use('/roomies/', productRoutes);
