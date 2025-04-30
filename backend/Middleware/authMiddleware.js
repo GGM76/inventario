1f767efa4862bb98e562bc68ca83fa6b556c44d2
Gabriel Garcia <g.garcia@DELL-33.seedgroup.local>
2025-04-30 12:27:49 -0600

    Revisando el back 3

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -2,31 +2,25 @@ const { admin } = require('../config/firebase');  // Accede correctamente a `adm
 
 const authenticateToken = async (req, res, next) => {
   const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-  console.log('Token recibido:', token);  // Log del token que llega
+  
 
   if (!token) {
-    console.log('No se proporcionó un token.');
     return res.status(401).json({ error: 'Token no proporcionado.' });
   }
 
   try {
     // Verificar token
-    console.log('Verificando token...');
     const decodedToken = await admin.auth().verifyIdToken(token);  
-    console.log('Token verificado:', decodedToken);  // Log del token verificado
 
     // Buscar el usuario en Firestore para obtener rol y empresa
-    console.log('Buscando usuario en Firestore con UID:', decodedToken.uid);
     const userRef = admin.firestore().collection('users').doc(decodedToken.uid);
     const userDoc = await userRef.get();
     
     if (!userDoc.exists) {
-      console.log('Usuario no encontrado en Firestore.');
       return res.status(404).json({ error: 'Usuario no encontrado.' });
     }
 
     const userData = userDoc.data();
-    console.log('Datos del usuario obtenidos:', userData);  // Log de los datos del usuario
 
     req.user = { ...decodedToken, role: userData.role, empresa: userData.empresa_id }; 
     next();  // Continuar a la siguiente función (controlador)
--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,34 +1,9 @@
-// const admin = require('firebase-admin');
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// let serviceAccount;
-
-// if (process.env.GOOGLE_CREDENTIALS) {
-//   try {
-//     const fixedCredentials = process.env.GOOGLE_CREDENTIALS.replace(/__LINE__/g, '\n');
-//     serviceAccount = JSON.parse(fixedCredentials);
-//   } catch (err) {
-//     console.error("❌ Error al parsear GOOGLE_CREDENTIALS:", err);
-//     process.exit(1);
-//   }
-// } else {
-//   serviceAccount = require('../inventario-11b49-firebase-adminsdk-fbsvc-2631f257be.json');
-// }
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
-// });
-
-// const db = admin.firestore();
-// module.exports = { admin, db };
 //config/firebase.js
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const serviceAccount = require('/etc/secrets/inventario-11b49-firebase-adminsdk-fbsvc-2631f257be.json'); // Cambia la ruta a /etc/secrets/
+const serviceAccount = require('/etc/secrets/inventario-11b49-firebase-adminsdk-fbsvc-2631f257be.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
