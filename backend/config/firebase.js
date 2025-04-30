153d33d3ea860942e5c51f1201740c1bf810ac90
Gabriel Garcia <g.garcia@DELL-33.seedgroup.local>
2025-04-30 12:13:20 -0600

    Revisando el back 2

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -22,16 +22,19 @@
 // });
 
 // const db = admin.firestore();
-
 // module.exports = { admin, db };
+//config/firebase.js
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const serviceAccount = require('/etc/secrets/inventario-11b49-firebase-adminsdk-fbsvc-2631f257be.json'); // Cambia la ruta a /etc/secrets/
+
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
+
 
 const db = admin.firestore();
 
