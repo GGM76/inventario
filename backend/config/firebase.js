ba425f012b2c0b7c3784b695e0c427249914aa72
GGM76 <ggmeraz76@gmail.com>
2026-05-20 16:32:40 -0600

    quitando variables

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -7,12 +7,12 @@ admin.initializeApp({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    clientId: process.env.FIREBASE_CLIENT_ID,
-    authUri: process.env.FIREBASE_AUTH_URI,
-    tokenUri: process.env.FIREBASE_TOKEN_URI,
-    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    // clientId: process.env.FIREBASE_CLIENT_ID,
+    // authUri: process.env.FIREBASE_AUTH_URI,
+    // tokenUri: process.env.FIREBASE_TOKEN_URI,
+    // authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    // clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    // universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   }),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
