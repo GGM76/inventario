037c113a1d5a43878f3d2efd3d0fd74590753342
GGM76 <ggmeraz76@gmail.com>
2026-05-20 14:28:42 -0600

    cambio de configuracion de variables

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,15 +1,15 @@
-//config/firebase.js
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const serviceAccount = require('./serviceAccount.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
-
 
 const db = admin.firestore();
 
